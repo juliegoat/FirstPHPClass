a471f0a73fcbe1f3d8b628b25eeef28e2e6c167c
Julie Gauthier <jg86588n@pace.edu>
2018-02-22 17:37:41 -0500

    Update files to include add_record and show_records examples

--- add_record.php
+++ add_record.php
@@ -1,22 +1,22 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+
 $link = mysqli_connect("localhost", "root", "root", "demo");
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
-// Attempt select query execution
-$sql = "INSERT INTO persons (first_name, last_name, email_address) VALUES ('John', 'Doe', 'john@example.com')";
 
-if($result = mysqli_query($link, $sql)){
-    echo "Succesfully added."
+ 
+// attempt insert query execution
+$sql = "INSERT INTO persons (first_name, last_name, Email_address) VALUES ('Wormy', 'VonWormenstein', 'email@email.com')";
+if(mysqli_query($link, $sql)){
+    echo "Records added successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
- 
 // Close connection
 mysqli_close($link);
 ?>
